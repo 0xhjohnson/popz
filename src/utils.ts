9b4361e357dacf951a4cc5e05202bae115c28102
Hunter Johnson <0xhjohnson@protonmail.com>
2023-12-26 21:56:02 -0500

    fix: edge case where nav item not selected when trailing slash

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,9 @@
 export function generateTitle(pageTitle: string) {
   return `${pageTitle} | Pop'z BBQ & Catering`
 }
+
+export function stripTrailingSlash(str: string) {
+  return str.endsWith('/') ?
+        str.slice(0, -1) :
+        str;
+}
